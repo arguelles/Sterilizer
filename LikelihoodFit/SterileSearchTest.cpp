ecd0b2432bef7de660662fff4260c577cf72a76e
benjpjones <ben.jones@uta.edu>
2017-01-14 21:06:56 -0600

    Adjust path

--- LikelihoodFit/SterileSearchTest.cpp
+++ LikelihoodFit/SterileSearchTest.cpp
@@ -9,6 +9,9 @@ int main(int argc, char* argv[]){
   SteeringParams  sp;
   SterileNuParams snp;
   
+  dp.mc_path="/data/user/bjones/Sterilizer/Sterilizer/test_data/";
+  dp.squids_files_path="/data/user/bjones/Sterilizer/Sterilizer/test_data/";
+
   std::cout<<"==Making an object with non-compact data =="<<std::endl<<std::endl;
   sp.ReadCompact=false;
   
@@ -18,29 +21,29 @@ int main(int argc, char* argv[]){
   
   std::cout<< "Getting the data distribution"<<std::endl;
   marray<double,3> DataDist=ster->GetDataDistribution();
-  std::cout<<"   Bin 1,1" << DataDist[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << DataDist[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << DataDist[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << DataDist[0][5][5] << std::endl;
 
   std::cout<< "Getting expectation for nominal nuisance"<<std::endl;
   Nuisance ns;
   std::cout<< " Normalization : " <<ns.normalization<<std::endl;
   
   marray<double,3> NominalExpectation=ster->GetExpectation(ns);
-  std::cout<<"   Bin 1,1" << NominalExpectation[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << NominalExpectation[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << NominalExpectation[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << NominalExpectation[0][5][5] << std::endl;
 
   std::cout<< "Getting expectation for adjusted nuisance"<<std::endl;
   ns.normalization=1.2;
   std::cout<< " Normalization : " <<ns.normalization<<std::endl;
   marray<double,3> AdjustedExpectation=ster->GetExpectation(ns);
-  std::cout<<"   Bin 1,1" << AdjustedExpectation[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << AdjustedExpectation[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << AdjustedExpectation[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << AdjustedExpectation[0][5][5] << std::endl;
 
   std::cout<<"Getting a realization for adjusted nuisance"<<std::endl;
   int seed=100;
   marray<double,3> Realization=ster->GetRealization(ns,seed);
-  std::cout<<"   Bin 1,1" << Realization[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << Realization[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << Realization[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << Realization[0][5][5] << std::endl;
 
   ns.normalization=1.0;
   std::cout<< " Normalization : " <<ns.normalization<<std::endl;
@@ -61,8 +64,8 @@ int main(int argc, char* argv[]){
 
   std::cout<< "Getting the data distribution"<<std::endl;
   DataDist=ster->GetDataDistribution();
-  std::cout<<"   Bin 1,1" << DataDist[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << DataDist[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << DataDist[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << DataDist[0][5][5] << std::endl;
  
 
   std::cout<<"Adjusting the sterile params to (1.0, 0.2)"<<std::endl;
@@ -72,13 +75,13 @@ int main(int argc, char* argv[]){
   
   std::cout<< "Getting the data distribution"<<std::endl;
   DataDist=ster->GetDataDistribution();
-  std::cout<<"   Bin 1,1" << DataDist[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << DataDist[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << DataDist[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << DataDist[0][5][5] << std::endl;
 
   std::cout<< "Getting expectation for nominal nuisance"<<std::endl;
   NominalExpectation=ster->GetExpectation(ns);
-  std::cout<<"   Bin 1,1" << NominalExpectation[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << NominalExpectation[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << NominalExpectation[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << NominalExpectation[0][5][5] << std::endl;
 
 
 
@@ -88,13 +91,13 @@ int main(int argc, char* argv[]){
 
   std::cout<< "Getting the data distribution"<<std::endl;
   DataDist=stersig->GetDataDistribution();
-  std::cout<<"   Bin 1,1" << DataDist[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << DataDist[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << DataDist[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << DataDist[0][5][5] << std::endl;
 
   std::cout<< "Getting expectation for nominal nuisance"<<std::endl;
   NominalExpectation=ster->GetExpectation(ns);
-  std::cout<<"   Bin 1,1" << NominalExpectation[0][1][1] << std::endl;
-  std::cout<<"   Bin 5,5" << NominalExpectation[0][5][5] << std::endl;
+  std::cout<<"   Bin 1,1 " << NominalExpectation[0][1][1] << std::endl;
+  std::cout<<"   Bin 5,5 " << NominalExpectation[0][5][5] << std::endl;
 
   std::cout<<"==== Sterilizer test module complete ===="<<std::endl<<std::endl;
 }
