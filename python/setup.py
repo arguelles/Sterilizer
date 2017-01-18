e02efffdf15a8510145a64a88aef0b452326b3e4
arguelles <arguelles@wisc.edu>
2017-01-17 23:29:23 -0600

    fix compilation and setup scripts

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
-import os.path
+import os,os.path
 import sys
 import numpy as np
 
@@ -10,27 +10,21 @@ if sys.platform == 'win32' or sys.platform == 'win64':
     quit()
 
 else:
-    include_dirs = ['/usr/local/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/lib/python2.7/../../include/python2.7',
-                    '/Users/carguelles/Library/Python/2.7/lib/python/site-packages/numpy/core/include',
-                    '/usr/local/include',np.get_include(),
+    include_dirs = [os.environ['SNOTBUILDPATH']+"/include",
+                    '/usr/local/include',
+                    np.get_include(),
                     '../Likelihood/',
                     '../LikelihoodFit/',
                     '.']
-    libraries = ['python2.7','boost_python',
+    libraries = ['python2.7','boost_python','LeptonWeighter',
                  'SQuIDS','nuSQuIDS',
                  'gsl','gslcblas','m',
                  'hdf5','hdf5_hl','PhysTools']
 
     if sys.platform.startswith('linux'):
-      libraries.append('cxxrt')
+      libraries.append('supc++')#'cxxrt'
 
-    library_dirs = ['/usr/local/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/lib/python2.7',
-                    '/usr/local/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/lib/python2.7/../',
-                    '/usr/local/lib',
-                    '/usr/local/lib',
-                    '/usr/local/Cellar/gsl/1.15/lib',
-                    '/usr/local/opt/szip/lib',
-                    '/usr/local/lib',
+    library_dirs = [os.environ['SNOTBUILDPATH']+"/lib",
                     '/usr/local/lib']
 
 files = ['SterileSearchPy.cpp']
@@ -41,7 +35,8 @@ setup(name = 'SterileSearchPy',
               library_dirs=library_dirs,
               libraries=libraries,
               include_dirs=include_dirs,
-              extra_objects=["../LikelihoodFit/lbfgsb.o","../LikelihoodFit/linpack.o","../LikelihoodFit/SterileSearch.o","../LikelihoodFit/analysisWeighting.o","../Likelihoodfit/dataIO.o","../LikelihoodFit/oversizeWeight.o"],
+              extra_objects=["../LikelihoodFit/analysisWeighting.o","../LikelihoodFit/Event.o","../LikelihoodFit/linpack.o","../LikelihoodFit/SterileSearch.o",
+                             "../LikelihoodFit/compactIO.o","../LikelihoodFit/lbfgsb.o","../LikelihoodFit/oversizeWeight.o"],
               extra_compile_args=['-O3','-fPIC','-std=c++11','-Wno-unused-local-typedef'],
               depends=[]),
           ]
