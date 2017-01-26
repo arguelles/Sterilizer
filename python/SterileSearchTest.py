90d6372fc7f1cdc5a3e724a5049c244b68264669
arguelles <arguelles@wisc.edu>
2017-01-26 13:55:47 -0600

    improved prompt flux handling

--- python/SterileSearchTest.py
+++ python/SterileSearchTest.py
@@ -4,13 +4,12 @@ import numpy as np
 # constructing a data path object
 
 dp=ssp.DataPaths()
+dp.squids_files_path="/home/carguelles/work/Sterilizer/conventional_fluxes/"
+dp.prompt_squids_files_path="/home/carguelles/work/Sterilizer/prompt_fluxes/"
 
 steer=ssp.SteeringParams()
 nus=ssp.SterileNuParams()
-
-dp.squids_files_path="/data/user/bjones/Sterilizer/Sterilizer/test_data/"
 steer.ReadCompact=False
 
 guy=ssp.Sterilizer(dp,steer,nus)
-
 
